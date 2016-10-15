137d78b15d6af9e0313d84ec123175f64ef6ea2e
KALPESH KRISHNA <kalpeshk2011@gmail.com>
2016-10-16 00:27:45 +0530

    Adding priority feedback loop

--- First/Components.vhd
+++ First/Components.vhd
@@ -2,7 +2,7 @@ library std;
 library ieee;
 use ieee.std_logic_1164.all;
 
-package ADCCComponents is
+package ProcessorComponents is
 
   component ClockDivider is
   port (
@@ -32,6 +32,28 @@ package ADCCComponents is
         Dout: out std_logic_vector(data_width-1 downto 0);
         clk, enable: in std_logic);
   end component DataRegister;
+
+  component PriorityEncoder is
+  port (
+    din: in std_logic_vector(7 downto 0);
+    dout: out std_logic_vector(2 downto 0)
+  );
+  end component PriorityEncoder;
+
+  component Demux is
+  port (
+    din: in std_logic_vector(2 downto 0);
+    dout: out std_logic_vector(7 downto 0)
+  );
+  end component Demux;
+
+  component PriorityLoop is
+  port (
+    input: in std_logic_vector(7 downto 0);
+    priority_select, clock: in std_logic;
+    output: out std_logic_vector(2 downto 0)
+  );
+  end component PriorityLoop;
 
 end package;
 
---
+++ First/Generators/priority_loop.py
@@ -0,0 +1,12 @@
+output = ""
+for i in range(0, 256):
+	binary = "{0:08b}".format(i)
+	output += binary + " " + str(binary.count('1')) + "\n"
+	for index in range(0,8):
+		j = binary[7-index]
+		if j == '1':
+			output += "{0:03b}".format(index) + "\n"
+
+f = open('../Tracefiles/tracefile_priority.txt', 'w')
+f.write(output)
+f.close()
